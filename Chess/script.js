0d1b351b40fb1f598041db48ec65806ef46f3cbe
Ando2306 <andobaloyan08@gmail.com>
2021-01-15 13:26:22 +0400

    App projects 1

---
+++ Chess/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// let sec = document.querySelector('section'), d, t = true;
+
+// for(let i = 0; i < 8; i++){
+// 	for(let j = 0; j < 8; j++){
+// 		d = document.createElement('div');
+// 		sec.appendChild(d)
+// 		d.classList.add('blok');
+
+// 		if(j === 0){
+// 			t = !t;
+// 		} 
+
+// 		if(t){
+// 			d.classList.add('black');
+// 		}
+// 		else{
+// 			d.classList.add('white');
+// 		}
+
+// 		t = !t;
+// 	}
+// }
+
+
+
+
